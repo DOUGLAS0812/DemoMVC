d0e9c1cb4a57d7ec4c7f69f71d821f48db906829
dougl <dougl@DESKTOP-H03ANNQ>
2025-01-09 22:58:58 -0300

    Aula de views

---
+++ Models/ViewComponents/SaudacaoAlunoViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace DemoMVC.Models.ViewComponents
+{
+    public class SaudacaoAlunoViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+
+        {
+            var aluno = new Aluno() { Name = "Douglas" };
+            return View(aluno);
+
+        }
+    }
+}
